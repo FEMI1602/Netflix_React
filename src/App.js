1e2de5eb03e6d3bd5e6f1e9f32b1007a4b9f12d2
femi <femi1602@gmail.com>
2021-05-29 12:37:08 +0530

    movies

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar';
 import Banner from './Components/Banner/Banner';
 import Rowpost from './Components/Rowpost/Rowpost';
+import {originals,action,comedy, horror, romance} from './urls'
 
 
 function App() {
@@ -10,7 +11,11 @@ function App() {
     <div className="App">
      <Navbar/>
      <Banner/>
-     <Rowpost/>
+     <Rowpost url={originals} title='Netflix Originals' />
+     <Rowpost url={action} title='Action Movies' isSmall />
+     <Rowpost url={comedy} title='Comedy Movies' isSmall />
+     <Rowpost url={horror} title='Horror Movies' isSmall />
+     <Rowpost url={romance} title='Romance Movies' isSmall />
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3'
+export const API_KEY= "c7853e4ae70b971229c57d9751e47fef"
+export const imageUrl='https://image.tmdb.org/t/p/original'
